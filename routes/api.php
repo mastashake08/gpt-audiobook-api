b44ea26f7ed7f54c96e4101e02a302cbc55c0b31
Hacker God <jyrone.parker@gmail.com>
2023-02-11 19:10:12 -0500

    debug

--- routes/api.php
+++ routes/api.php
@@ -44,9 +44,11 @@ Route::get('/get-key', function() {
 Route::post('/generate-story', function (Request $request) {
   $result = OpenAI::completions()->create([
     'model' => 'text-davinci-003',
-    'prompt' => `generate a 10 page children's story with a title and a 512x512 png image. Return a well formatted JSON object with a title property that contains the title, an img property that contains link to the image generated, a text property that contains the story and ssml property that contains an well formatted SSML file prefixing <?xml version="1.0" ?><speak version="1.1" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US"> generated from the story serialized as a string. Serialized the JSON object as a string.`,
+    'prompt' => 'generate a 10 page children's story with a title and a 512x512 png image. Return a well formatted JSON object with a title property that contains the title, an img property that contains link to the image generated, a text property that contains the story and ssml property that contains an well formatted SSML document generated from the story serialized as a string. Serialized the JSON object as a string.',
+    'temperature' => 1,
+    'max_tokens' => 2048
  ]);
-
+ var_dump($result['choices'][0]['text']);
  $story = Story::Create([
    'story_data' => $result['choices'][0]['text']
  ]);
