0a96cdb217a639d2e1c9add080418e74f307ed45
Hacker God <jyrone.parker@gmail.com>
2023-02-11 19:19:14 -0500

    remove var_dump

--- app/Http/Resources/StoryResource.php
+++ app/Http/Resources/StoryResource.php
@@ -14,7 +14,6 @@ class StoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        var_dump($this->story_data);
         return [
           'title' => $this->story_data['title'],
           'text' => $this->story_data['text'],
